c1900ecd2b40dc73aa0354995952b482bbc1b10e
Harshal <harshaldesai01@gmail.com>
2024-09-16 22:20:42 +0100

    initial commit

---
+++ src/main/scala/Direction.scala
@@ -0,0 +1,7 @@
+/**
+ * Direction enum representing North, South, East and West directions with N, S, E & W respectively
+ */
+enum Direction {
+  case N, S, E, W
+
+}
---
+++ src/main/scala/main.scala
@@ -0,0 +1,21 @@
+/**
+ * Main function to take inputs, initialize relevant classes, run simulation and print the results
+ */
+@main
+def main(): Unit = {
+  println("Enter dimension of the Grid (>=1):")
+  var gridSize: Int = scala.io.StdIn.readInt()
+  if(gridSize < 1) throw new IllegalArgumentException("Grid size must be at least 1.")
+  
+  
+  // Start simulation
+  simulate()
+
+  /**
+   * Method to initialize skippy and simulate his hops home
+   */
+  def simulate(): Unit = {
+    
+  }
+}
+
